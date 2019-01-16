ef1272601ff3309a386fb8690b326ea52a7c6923
LucasLeandro1204 <lucasleandro1204@gmail.com>
2019-01-16 17:46:03 -0200

    base docs

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,85 @@
+module.exports = {
+  title: 'Vue Content Loading',
+  description: 'Vue component to easily build (or use presets) SVG loading cards Facebook like.',
+  head: [
+    ['meta',
+      {
+        hid: 'description',
+        name: 'description',
+        content: 'Vue component to easily build (or use presets) SVG loading cards Facebook like.'
+      },
+    ],
+    ['meta',
+      {
+        name: 'theme-color',
+        content: '#42b983'
+      },
+    ],
+    ['link',
+      {
+        rel: 'manifest',
+        href: '/icons/manifest.json',
+      },
+    ],
+    ['link',
+      {
+        rel: 'icon',
+        type: 'image/png',
+        href: '/icons/favicon-32.png',
+        sizes: '32x32'
+      },
+    ],
+    ['link',
+      {
+        rel: 'icon',
+        type: 'image/png',
+        href: '/icons/favicon-62.png',
+        sizes: '62x62'
+      },
+    ],
+    ['link',
+      {
+        rel: 'icon',
+        type: 'image/png',
+        href: '/icons/favicon-128.png',
+        sizes: '128x128'
+      },
+    ],
+    ['link',
+      {
+        rel: 'icon',
+        type: 'image/png',
+        href: '/icons/favicon-196.png',
+        sizes: '196x196'
+      },
+    ],
+    ['link',
+      {
+        rel: 'apple-touch-icon',
+        href: '/icons/favicon-120.png',
+        sizes: '120x120'
+      },
+    ],
+    ['link',
+      {
+        rel: 'apple-touch-icon',
+        href: '/icons/favicon-180.png',
+        sizes: '180x180'
+      },
+    ],
+    ['link',
+      {
+        rel: 'apple-touch-icon',
+        href: '/icons/favicon-152.png',
+        sizes: '152x152'
+      },
+    ],
+    ['link',
+      {
+        rel: 'apple-touch-icon',
+        href: '/icons/favicon-167.png',
+        sizes: '167x167'
+      },
+    ],
+  ],
+};
